b156c9cb18c89356a0dd9ea328b40f04bb75529d
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-17 20:57:15 +0200

    initial commit

---
+++ src/main/java/com/sistemaexamenes/backend/ExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaexamenes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenesApplication.class, args);
+    }
+
+}
